084586838c35f5e596918e1fd2065513826cb553
faceless0124 <57837155+faceless0124@users.noreply.github.com>
2023-12-06 23:33:35 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -22,9 +22,10 @@ class MyDataset(Dataset):
             self.sampleIdx_ls = data[3::4]
         self.length = len(self.traces_ls)
         print(self.data_path)
-        print(self.length)
-        print(len(self.roads_ls))
+        print(len(self.traces_ls), len(self.roads_ls), len(self.candidates), len(self.sampleIdx_ls))
         assert len(self.traces_ls) == len(self.roads_ls)
+        assert len(self.traces_ls) == len(self.candidates)
+        assert len(self.traces_ls) == len(self.sampleIdx_ls)
 
     def __getitem__(self, index):
         return self.traces_ls[index], self.roads_ls[index],\
@@ -35,15 +36,15 @@ class MyDataset(Dataset):
 
 def padding(batch):
     trace_lens = [len(sample[0]) for sample in batch]
-    road_lens = [len(sample[1]) for sample in batch]
-    max_tlen, max_rlen = max(trace_lens), max(road_lens)
+    # road_lens = [len(sample[1]) for sample in batch]
+    # max_tlen, max_rlen = max(trace_lens), max(road_lens)
+    max_len = max(trace_lens)
     x, y, z, w = [], [], [], []
     # 0: [PAD]
     for sample in batch:
-        x.append(sample[0] + [0] * (max_tlen - len(sample[0])))
-        y.append(sample[1] + [-1] * (max_rlen - len(sample[1])))
-        z.append(sample[2] + [[0, 0]] * (max_tlen - len(sample[2])))
-        w.append(sample[3] + [-1] * (max_tlen - len(sample[3])))
-    f = torch.LongTensor
-    print(type(x), type(y), type(z), type(w), type(trace_lens), type(road_lens))
-    return f(x), f(y), torch.FloatTensor(z), f(w), trace_lens, road_lens
+        x.append(sample[0] + [[0, 0]] * (max_len - len(sample[0])))
+        y.append(sample[1] + [-1] * (max_len - len(sample[1])))
+        z.append(sample[2] + [[0, 0]] * (max_len - len(sample[2])))
+        w.append(sample[3] + [-1] * (max_len - len(sample[3])))
+    # print(type(x), type(y), type(z), type(w), type(trace_lens), type(road_lens))
+    return torch.FloatTensor(x), torch.LongTensor(y), torch.FloatTensor(z), torch.LongTensor(w), trace_lens, road_lens
