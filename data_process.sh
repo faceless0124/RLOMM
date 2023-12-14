2572922449d0be11825596af53e368f9de1db0d1
faceless0124 <57837155+faceless0124@users.noreply.github.com>
2023-12-15 00:37:27 +0800

    update

--- data_process.sh
+++ data_process.sh
@@ -1,5 +1,11 @@
 cd data_preprocess
 
+python build_road_graph.py
+echo 'finish build_road_graph'
+
+python build_A.py
+echo 'finish build_A'
+
 downsample_rate=0.5
 
 python data_process.py $downsample_rate
