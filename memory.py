c7a5242289638689e2e6462d63b6dcb9814344fb
faceless0124 <57837155+faceless0124@users.noreply.github.com>
2023-12-15 00:37:33 +0800

    update

--- memory.py
+++ memory.py
@@ -1,14 +1,21 @@
 from collections import namedtuple, deque
 import random
 
+# 定义一个新的 namedtuple，包括 trace, matched_road_segments_id, 和 candidates
+State = namedtuple('State', ('trace', 'matched_road_segments_id', 'candidates'))
+
+# 修改 Transition，以使用新的 State 结构
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class Memory(object):
     def __init__(self, capacity):
-        self.memory = deque([],maxlen=capacity)
+        self.memory = deque([], maxlen=capacity)
 
-    def push(self, *args):
-        self.memory.append(Transition(*args))
+    def push(self, trace, matched_road_segments_id, candidates, action, next_trace, next_matched_road_segments_id, next_candidates, reward):
+        # 使用 State namedtuple 创建 state 和 next_state
+        state = State(trace, matched_road_segments_id, candidates)
+        next_state = State(next_trace, next_matched_road_segments_id, next_candidates)
+        self.memory.append(Transition(state, action, next_state, reward))
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
