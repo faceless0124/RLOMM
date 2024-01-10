73bfb41bca157ff2a402c21a99310a413a981a49
faceless0124 <57837155+faceless0124@users.noreply.github.com>
2024-01-10 14:15:10 +0800

    update

--- data_loader.py
+++ data_loader.py
@@ -5,6 +5,7 @@ import torch
 import data_preprocess.utils as utils
 import time
 
+
 class MyDataset(Dataset):
     def __init__(self, path, name):
         if not path.endswith('/'):
@@ -12,7 +13,6 @@ class MyDataset(Dataset):
         self.data_path = osp.join(path, f"{name}_data/{name}.json")
         self.MIN_LAT, self.MIN_LNG, self.MAX_LAT, self.MAX_LNG = utils.get_border('./data/road.txt')
         self.buildingDataset(self.data_path)
-
 
     def buildingDataset(self, data_path):
         with open(data_path, "r") as fp:
@@ -34,12 +34,12 @@ class MyDataset(Dataset):
 
         print(self.data_path)
         print("Total points: ", self.cnt)
-        print(len(self.traces_ls), len(self.time_stamps), len(self.roads_ls), len(self.candidates), len(self.candidates_id))
+        print(len(self.traces_ls), len(self.time_stamps), len(self.roads_ls), len(self.candidates),
+              len(self.candidates_id))
         assert len(self.traces_ls) == len(self.roads_ls)
         assert len(self.traces_ls) == len(self.candidates)
         assert len(self.traces_ls) == len(self.candidates_id)
         assert len(self.traces_ls) == len(self.time_stamps)
-
 
     def count_point(self):
         count = 0
@@ -80,20 +80,12 @@ class MyDataset(Dataset):
         return normalized_time_stamps
 
     def __getitem__(self, index):
-        return self.traces_ls[index], self.time_stamps[index], self.roads_ls[index],\
+        return self.traces_ls[index], self.time_stamps[index], self.roads_ls[index], \
             self.candidates[index], self.candidates_id[index]
 
     def __len__(self):
         return self.length
 
-
-    def get_data(self, index):
-        if index >= self.length:
-            raise IndexError("索引超出数据集范围")
-        return self.__getitem__(index)
-
-    def get_length(self):
-        return self.length
 
 def padding(batch):
     trace_lens = [len(sample[0]) for sample in batch]
@@ -106,21 +98,10 @@ def padding(batch):
         traces.append(sample[0] + [[0, 0]] * (max_tlen - len(sample[0])))
         time_stamp.append(sample[1] + [-1] * (max_tlen - len(sample[1])))
         roads.append(sample[2] + [-1] * (max_tlen - len(sample[2])))
-        candidates.append([candidates + [[0, 0]] * (max_clen-len(candidates)) for candidates in sample[3]] + [[[0, 0]] * max_clen] * (max_tlen - len(sample[3])))
-        candidates_id.append([candidates_id + [8533] * (max_clen-len(candidates_id)) for candidates_id in sample[4]] + [[-1] * max_clen] * (max_tlen - len(sample[4])))
+        candidates.append([candidates + [[0, 0]] * (max_clen - len(candidates)) for candidates in sample[3]] + [
+            [[0, 0]] * max_clen] * (max_tlen - len(sample[3])))
+        candidates_id.append(
+            [candidates_id + [8533] * (max_clen - len(candidates_id)) for candidates_id in sample[4]] + [
+                [-1] * max_clen] * (max_tlen - len(sample[4])))
     traces = torch.cat((torch.FloatTensor(traces), torch.FloatTensor(time_stamp).unsqueeze(-1)), dim=-1)
     return traces, torch.LongTensor(roads), torch.FloatTensor(candidates), torch.LongTensor(candidates_id), trace_lens
-
-# def padding(batch):
-#     trace_lens = [len(sample[0]) for sample in batch]
-#     # road_lens = [len(sample[1]) for sample in batch]
-#     candidates_lens = [len(candidates) for sample in batch for candidates in sample[2]]
-#     max_tlen, max_clen = max(trace_lens), max(candidates_lens)
-#     traces, roads, candidates, candidates_id = [], [], [], []
-#     # 0: [PAD]
-#     for sample in batch:
-#         traces.append(sample[0] + [[0, 0]] * (max_tlen - len(sample[0])))
-#         roads.append(sample[1] + [-1] * (max_tlen - len(sample[1])))
-#         candidates.append([candidates + [[0, 0]] * (max_clen-len(candidates)) for candidates in sample[2]] + [[[0, 0]] * max_clen] * (max_tlen - len(sample[2])))
-#         candidates_id.append([candidates_id + [-1] * (max_clen-len(candidates_id)) for candidates_id in sample[3]] + [[-1] * max_clen] * (max_tlen - len(sample[3])))
-#     return torch.FloatTensor(traces), torch.LongTensor(roads), torch.FloatTensor(candidates), torch.LongTensor(candidates_id), trace_lens
