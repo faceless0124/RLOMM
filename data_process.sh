f022883daea4b3449a16dc8d7f4b1de74d4bab50
faceless0124 <57837155+faceless0124@users.noreply.github.com>
2024-05-09 17:23:54 +0800

    update

--- data_process.sh
+++ data_process.sh
@@ -1,21 +1,23 @@
 cd data_preprocess
 
-python build_road_graph.py
+city='porto'
+
+python build_road_graph.py $city
 echo 'finish build_road_graph'
+
+#python build_road_dis.py $city
+#echo 'finish build_road_dis'
 
 downsample_rate=0.5
 
-python build_road_dis.py $downsample_rate
-echo 'finish build_road_dis'
+#python data_process.py $downsample_rate $city
+#echo 'finish data_process'
 
-python data_process.py $downsample_rate
-echo 'finish data_process'
-
-python build_trace_graph.py $downsample_rate
+python build_trace_graph.py $downsample_rate $city
 echo 'finish build_trace_graph'
 
-python maproad2grid.py $downsample_rate
+python maproad2grid.py $downsample_rate $city
 echo 'finish maproad2grid'
 
-python build_grid_road_matrix.py $downsample_rate
+python build_grid_road_matrix.py $downsample_rate $city
 echo 'finish build_grid_road_matrix'
